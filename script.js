78bd764f6b0fd180cf38c562c2451b8bd5d4aef1
Lachie David <lachiepd@gmail.com>
2023-05-09 17:27:26 +1000

    polished a bit, just about done for now

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 const secondsId = document.getElementById("seconds");
+const minutesId = document.getElementById("minutes");
+const hoursId = document.getElementById("hours");
+
 let running = false;
 let intervalId; 
 const start = () =>{
 	try{
 		if(!running)
 		{
-			console.log("initiated")
 			let secondsInt = parseInt(secondsId.innerText);
+			let minutesInt = parseInt(minutesId.innerText);
+			let hoursInt = parseInt(hoursId.innerText);
+			
 			intervalId = setInterval(()=>{
 				secondsInt++;
 				secondsId.innerText = secondsInt;
-			}, 1000);
+
+				if(secondsInt > 59){
+					secondsInt = 0;
+					secondsId.innerText = secondsInt;
+					minutesInt++;
+					minutesId.innerText = minutesInt;
+				}
+			}, 10);
 			running =true;
 		}
 	}catch(err){console.log(err)}
